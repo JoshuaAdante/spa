b1a80330e1646767c927ee956f33ffea1c4340c8
Trubias <jay.trubias@urios.edu.ph>
2025-03-27 03:30:18 +0000

    first commit

---
+++ script/app.js
@@ -0,0 +1,14 @@
+
+
+Alpine.store("settings", {
+    apiBaseUrl:"http://172.17.100.14:3329/trubias2/api",  
+    appName: "contact application",
+    
+})
+    
+Alpine.store("settings", {
+    apiBaseUrl:"http://172.17.100.14:3329/trubias2/api/exam",  
+    appName: "chat application",  
+    
+   
+});
