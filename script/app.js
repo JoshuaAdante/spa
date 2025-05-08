10ef8ad7a12a87215b4bb99897b095cc40cad461
Trubias <jay.trubias@urios.edu.ph>
2025-05-08 02:48:54 +0000

    project spa

--- script/app.js
+++ script/app.js
@@ -1,14 +1,14 @@
 
 
 Alpine.store("settings", {
-    apiBaseUrl:"http://172.17.100.14:3329/trubias2/api",  
+    apiBaseUrl:"http://172.17.100.14:3442/trubias2/api",  
     appName: "contact application",
     
-})
-    
+});
+
 Alpine.store("settings", {
-    apiBaseUrl:"http://172.17.100.14:3329/trubias2/api/exam",  
-    appName: "chat application",  
-    
-   
+    apiBaseUrl:"http://172.17.100.14:3442/trubias2/api/store",  
+    appName: "products application",  
+       
+  
 });
